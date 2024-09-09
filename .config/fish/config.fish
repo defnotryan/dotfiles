0cf8e9d52b303ccfcb6e5d4e417283cf148c7e21
Ryan Burger <ryan@theredremedy.com>
2024-09-08 21:31:19 -0400

    switch to tokyo night theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source {$HOME}/.config/fish/themes/nightfox.fish
+    source {$HOME}/.config/fish/themes/tokyonight_night.fish
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
