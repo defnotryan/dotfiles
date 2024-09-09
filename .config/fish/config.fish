256735b1af6fd6769227880cd1aa1a0964f6b647
Ryan Burger <ryan@theredremedy.com>
2024-09-08 21:55:26 -0400

    switch to nightfox theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source {$HOME}/.config/fish/themes/tokyonight_night.fish
+    #source {$HOME}/.config/fish/themes/tokyonight_night.fish
+    source {$HOME}/.config/fish/themes/nightfox.fish
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
