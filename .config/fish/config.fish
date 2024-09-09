448f7964418ddbd20976df27d983cddca6ec5d21
Ryan Burger <ryan@theredremedy.com>
2024-09-08 21:07:57 -0400

    try 16 color tide config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source {$HOME}/.config/fish/themes/nightfox.fish
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 fzf --fish | source
-
-source {$HOME}/.config/fish/themes/nightfly.fish
-
-set --global tide_pwd_bg_color $fish_color_command
